474bf850252ed0c5f9da1ed544068a8c7a95adfa
chrischris292 <cbchan2@illinois.edu>
2014-09-05 17:54:02 -0500

    fdsfsdfdsf

--- js/custom.js
+++ js/custom.js
@@ -34,12 +34,6 @@
         $(".thirdsColumn").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",function(){
             $(this).removeClass('animated fadeInDown ' + $(this).data('action'));
         });
-        $(".topHeader > h1").hover(function(){
-            $(this).addClass('animated tada');
-        });
-        $(".topHeader > h1").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",function(){
-            $(this).removeClass('animated tada');
-        });
         $(".aboutMee > a > .title").click(function(){
              $('html,body').animate({scrollTop: $(this).offset().top}, 800);
         })
